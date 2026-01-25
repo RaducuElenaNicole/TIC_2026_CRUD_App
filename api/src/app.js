40cabd10d12155fd0d86657332bd14d66ab6eae1
elena.nicole.raducu@gmail.com <elena.nicole.raducu@gmail.com>
2026-01-26 00:07:57 +0200

    refactor: reorganize app setup

--- api/src/app.js
+++ api/src/app.js
@@ -18,11 +18,18 @@ app.use((req, res, next) => {
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '..', '..', 'client')));
+const CLIENT_DIR = path.resolve(__dirname, "../../client");
+app.use(express.static(CLIENT_DIR));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(CLIENT_DIR, "index.html"));
+});
 
+/*
+app.use(express.static(path.join(__dirname, '..', '..', 'client')));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "..", "client", "index.html"));
 });
+*/
 
 app.get('/api', (req, res) => {
   res.json({ message: 'API is running' });
